519beb2b4068b3749cf1e4535379833bcf910f88
VladimirImir <mimimvvv@gmail.com>
2023-10-07 23:19:46 +0300

    Groovy. 4. - ClassLoader. ClassPath.

--- src/com/dev/HelloWorld.groovy
+++ src/com/dev/HelloWorld.groovy
@@ -1,8 +1,16 @@
 package com.dev
 
+import java.sql.DriverManager
+
 class HelloWorld {
 
     static void main(String[] args) {
+        String value = "Hello World"
+        Class<String> clazz = value.getClass()
+        assert clazz == String.class
         println ("HelloWorld")
+        println clazz.getClassLoader()
+        println DriverManager.class.getClassLoader()
+        println HelloWorld.class.getClassLoader()
     }
 }
