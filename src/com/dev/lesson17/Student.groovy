9949e0f30c291328ea64c68080f3f2b65e528d20
VladimirImir <mimimvvv@gmail.com>
2023-10-16 14:45:45 +0300

    Groovy. 20. - AST transformations.

--- src/com/dev/lesson17/Student.groovy
+++ src/com/dev/lesson17/Student.groovy
@@ -1,11 +1,21 @@
 package com.dev.lesson17
 
 import com.dev.lesson18.WithId
+import groovy.transform.Canonical
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.Immutable
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
+import groovy.transform.builder.Builder
+import groovy.util.logging.Slf4j
 
 @ToString
 @TupleConstructor
+@EqualsAndHashCode
+//@Canonical
+@Immutable
+@Builder
+//@Slf4j
 class Student implements WithId {
     String firstName
     String lastName
@@ -13,5 +23,12 @@ class Student implements WithId {
 
     def getAt(Integer index) {
         index == 0 ? firstName : lastName
+    }
+
+    static void main(String[] args) {
+        /*Student.builder()
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .build()*/
     }
 }
